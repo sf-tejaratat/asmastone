17a96280d1e9bba2b3524e4f23beeebe3b0cb44e
nemesisko <kaveh.zhian@gmail.com>
2021-01-09 00:31:14 +0330

    change jquery cdn to file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,8 +17,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> --}}
+    <script src="/js/jquery3.5.min.js" ></script>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     {{-- <link rel="stylesheet" href="/css/bundle.css "> --}}
